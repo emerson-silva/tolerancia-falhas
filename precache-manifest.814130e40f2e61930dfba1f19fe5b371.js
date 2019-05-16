6c33b4b77ca96a2899ff4533bbb1f9e77c003696
Emerson Silva <email.emerson.silva@gmail.com>
2019-05-16 16:41:07 -0300

    Updates

---
+++ precache-manifest.814130e40f2e61930dfba1f19fe5b371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e2783c8423fe34f760e5ebae039149",
+    "url": "/tolerancia-falhas/index.html"
+  },
+  {
+    "revision": "1855947e2cc15e6fe28f",
+    "url": "/tolerancia-falhas/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "805f948d3870d6e30eb5",
+    "url": "/tolerancia-falhas/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1855947e2cc15e6fe28f",
+    "url": "/tolerancia-falhas/static/js/2.b1d8aaaf.chunk.js"
+  },
+  {
+    "revision": "805f948d3870d6e30eb5",
+    "url": "/tolerancia-falhas/static/js/main.ce1762e6.chunk.js"
+  },
+  {
+    "revision": "32d2367a34a9ac602091",
+    "url": "/tolerancia-falhas/static/js/runtime~main.0fa45cd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tolerancia-falhas/static/media/logo.5d5d9eef.svg"
+  }
+]);
